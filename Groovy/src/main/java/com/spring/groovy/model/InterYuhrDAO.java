c9e60a75e8beacc690b1f20d2822d9ec289e1abf
Helenhyerim hr4658@naver.com <sist@LAPTOP-U5OIAH7T>
2022-04-29 15:52:53 +0900

    사원등록 작업중

--- Groovy/src/main/java/com/spring/groovy/model/InterYuhrDAO.java
+++ Groovy/src/main/java/com/spring/groovy/model/InterYuhrDAO.java
@@ -1,6 +1,18 @@
 package com.spring.groovy.model;
 
+import java.util.List;
+import java.util.Map;
+
 public interface InterYuhrDAO {
-	
+
+	// 부서명을 가져오기 위함
+	List<DepartmentVO> getDepts();
+
+	// 직위명을 가져오기 위함
+	List<SpotVO> getSpots();
+
+	// 사원번호 입력칸 유효성검사(중복검사, ajax 로 처리)
+	String empnumCheck(Map<String, String> paraMap);
+
 
 }//end of public interface InterYuhrDAO
