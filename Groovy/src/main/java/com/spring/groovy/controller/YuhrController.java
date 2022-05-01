bfd6fe24531a16a5525fd4e8cbf2bd5cf7ddc550
Helenhyerim hr4658@naver.com <sist@LAPTOP-U5OIAH7T>
2022-05-01 09:53:44 +0900

    사원조회 및 근태관리 페이지 작업중

--- Groovy/src/main/java/com/spring/groovy/controller/YuhrController.java
+++ Groovy/src/main/java/com/spring/groovy/controller/YuhrController.java
@@ -1,5 +1,6 @@
 package com.spring.groovy.controller;
 
+import java.io.File;
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +12,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.groovy.common.*;
 import com.spring.groovy.model.DepartmentVO;
+import com.spring.groovy.model.EmployeeVO;
 import com.spring.groovy.model.SpotVO;
 import com.spring.groovy.service.*;
 
@@ -82,15 +85,141 @@ public class YuhrController {
 		return jsonObj.toString();
 	}
 	
-	@RequestMapping(value="/registerEnd.groovy", method= {RequestMethod.POST})
-	public ModelAndView registerEnd(Map<String,String> paraMap, ModelAndView mav, MultipartHttpServletRequest mrequest) {
+	
+	// 사원등록 요청
+	@RequestMapping(value="/registerEnd.groovy", method= {RequestMethod.POST}, produces="text/plain;charset=UTF-8")
+	public ModelAndView registerEnd(Map<String,String> paraMap, ModelAndView mav, EmployeeVO empVo, MultipartHttpServletRequest mrequest) {
+
+		MultipartFile attach = empVo.getAttach();
+		if(!attach.isEmpty()) { // attach(첨부파일)이 있다면
+			/*
+	            1. 사용자가 보낸 첨부파일을 WAS(톰캣)의 특정 폴더에 저장해주어야 한다. 
+	            >>> 파일이 업로드 되어질 특정 경로(폴더)지정해주기
+	                       우리는 WAS의 webapp/resources/files 라는 폴더로 지정해준다.
+	                       조심할 것은  Package Explorer 에서  files 라는 폴더를 만드는 것이 아니다.       
+	        */
+			
+			// WAS 의 webapp 의 절대경로를 알아와야 한다.
+			HttpSession session = mrequest.getSession();
+			String root = session.getServletContext().getRealPath("/");
+			
+		//	System.out.println("~~ 확인용 webapp의 절대경로 =>" +root);
+			// ~~ 확인용 webapp의 절대경로 =>C:\NCS\workspace(spring)\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\Groovy\
+
+			String path = root + "resources"+File.separator+"files";
+			/* File.separator 는 운영체제에서 사용하는 폴더와 파일의 구분자이다.
+		             운영체제가 Windows 이라면 File.separator 는  "\" 이고,
+		             운영체제가 UNIX, Linux 이라면  File.separator 는 "/" 이다. 
+			*/
+			
+			// path 가 청부파일이 저장될 WAS(톰캣)의 폴더가 된다.
+		//	System.out.println("~~ 확인용 path =>" +path);
+			// ~~ 확인용 path =>C:\NCS\workspace(spring)\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\Groovy\resources\files
+
+			/*
+		    	2. 파일첨부를 위한 변수의 설정 및 값을 초기화 한 후 파일올리기   
+			 */
+			String newFileName = "";
+			// WAS(톰캣)의 디스크에 저장될 파일명 
+			
+			byte[] bytes = null;
+			// 첨부파일의 내용물을 담는 것  
+			
+			try {
+				bytes = attach.getBytes();
+				// 첨부파일의 내용물을 읽어오는 것 
+				
+				newFileName = fileManager.doFileUpload(bytes, attach.getOriginalFilename(), path); 
+				// 첨부되어진 파일을 업로드 하도록 하는 것이다. 
+				// attach.getOriginalFilename() 은 첨부파일의 파일명(예: 강아지.png)이다.
+				
+			//	System.out.println(">>> 확인용  newFileName => " + newFileName);
+				// >>> 확인용  newFileName => 20220429202622949927811335000.jpg
+
+				
+			/*
+			    3. BoardVO boardvo 에 fileName 값과 orgFilename 값과 fileSize 값을 넣어주기   
+			*/
+				empVo.setEmppicturefilename(newFileName);
+				// WAS(톰캣)에 저장될 파일명(2020120809271535243254235235234.png) 
+				
+				empVo.setEmppicturename(attach.getOriginalFilename());
+				// 게시판 페이지에서 첨부된 파일(강아지.png)을 보여줄 때 사용.
+				// 또한 사용자가 파일을 다운로드 할때 사용되어지는 파일명으로 사용.
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
+		
+		int n = 0;
+		
+		if(attach.isEmpty()) {
+			// 파일첨부가 없는 경우라면
+			empVo.setEmppicturename("");
+			n = service.addEmp(empVo);
+		}
+		else {
+			// 파일첨부가 있는 경우라면
+			n = service.addEmp(empVo);
+		}
+		
+		if(n==1) {
+			mav.addObject("message", "사원이 성공적으로 등록되었습니다.");
+			mav.addObject("loc", mrequest.getContextPath()+"/index.groovy");
+			
+			mav.setViewName("msg");
+		}
+		else {
+			mav.addObject("message", "사원 등록에 실패했습니다.");
+			mav.addObject("loc", "javascript:history.back()");
+			
+			mav.setViewName("msg");
+		}
 		
 		return mav;
 	}
 
-		
 	
+	// 조직도 및 사원정보 조회 페이지
+	@RequestMapping(value ="/viewEmp.groovy")
+	public ModelAndView viewEmp(ModelAndView mav) {
+		
+		// 부서정보을 가져오기 위함
+		List<DepartmentVO> departments = service.getDepts();
+		
+		// 직위정보을 가져오기 위함
+		List<SpotVO> spots = service.getSpots();
+				
+		mav.addObject("departments", departments);
+		mav.addObject("spots", spots);
+		
+		mav.setViewName("employee/viewEmp.tiles1");
+		
+		return mav;//  /WEB-INF/views/tiles1/employee/viewEmp.jsp 페이지 만들어야 한다.
+		
+	}//end of public ModelAndView viewEmp(ModelAndView mav)	
+	
+	
+	// 근태관리 페이지(사원용 및 관리자용)
+	@RequestMapping(value ="/worktime.groovy")
+	public ModelAndView worktimeA(ModelAndView mav) {
+		
+		// 부서정보을 가져오기 위함
+		List<DepartmentVO> departments = service.getDepts();
+		
+		// 직위정보을 가져오기 위함
+		List<SpotVO> spots = service.getSpots();
+		
+		mav.addObject("departments", departments);
+		mav.addObject("spots", spots);
+		
+		mav.setViewName("employee/worktime.tiles1");
+		
+		return mav;//  /WEB-INF/views/tiles1/employee/worktime.jsp 페이지 만들어야 한다.
+		
+	}//end of public ModelAndView viewEmp(ModelAndView mav)	
 	
 	
 	
