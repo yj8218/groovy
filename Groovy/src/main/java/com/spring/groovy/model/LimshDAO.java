bcec2c2ee71429098836c6ed5084ad951ced8636
lsh87 <stjdgml6763@naver.com>
2022-05-09 14:06:36 +0900

    개인브랜치 커밋

--- Groovy/src/main/java/com/spring/groovy/model/LimshDAO.java
+++ Groovy/src/main/java/com/spring/groovy/model/LimshDAO.java
@@ -1,6 +1,7 @@
 package com.spring.groovy.model;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -21,6 +22,46 @@ public class LimshDAO implements InterLimshDAO {
 		List<EmployeeVO> empvoList = sqlsession.selectList("limsh.showEmployeeList");
 		
 		return empvoList;
+	}
+	
+	
+	// 검색한 프로젝트 참여자 목록 보여주기(select)
+	@Override
+	public List<EmployeeVO> searchEmp(Map<String, String> paraMap) {
+
+		List<EmployeeVO> empvoList = sqlsession.selectList("limsh.searchEmp", paraMap);
+		
+		return empvoList;
+	}
+
+
+	// 사용자 프로필 보여주기(ajax) : 사진, 이름, 이메일, 전화번호
+	@Override
+	public EmployeeVO showEmpProfile(String pk_empnum) {
+		
+		EmployeeVO empvo = sqlsession.selectOne("limsh.showEmpProfile", pk_empnum);
+		
+		return empvo;
+	}
+
+
+	// 부서 목록 가져오기(select)
+	@Override
+	public List<DepartmentVO> getDeptvoList() {
+
+		List<DepartmentVO> deptvoList = sqlsession.selectList("limsh.getDeptvoList");
+		
+		return deptvoList;
+	}
+
+
+	// 부서별 직원 목록 가져오기(select)
+	@Override
+	public List<EmployeeVO> getEmpListByDept(String pk_deptnum) {
+		
+		List<EmployeeVO> empvoListByDept = sqlsession.selectList("limsh.getEmpListByDept", pk_deptnum);
+		
+		return empvoListByDept;
 	}
 
 
